12405798fe89f37b8b90b409a033dff5dc63422e
Gabriel Santino <ggs2209@gmail.com>
2024-04-10 14:36:16 -0300

    first commit

---
+++ src/main/java/com/api/questionario/QuestionarioApplication.java
@@ -0,0 +1,13 @@
+package com.api.questionario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionarioApplication.class, args);
+	}
+
+}
